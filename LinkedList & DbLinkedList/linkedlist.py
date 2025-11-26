3124a3ad173cded4daad1be3c605e905eceeefbf
Tarun Kannadas <tarunkannadas@gmail.com>
2025-11-26 16:14:50 +0530

    commit

--- LinkedList & DbLinkedList/linkedlist.py
+++ LinkedList & DbLinkedList/linkedlist.py
@@ -4,14 +4,10 @@ class Node:
       self.next = None
 
 class addVal:
-   def __init__(self):
-      self.head = None
-
-   def listprint(self):
+   def listing(self):
       printval = self.head
-      print("\nLinked List is: ")
       while printval is not None:
-         print(printval.data, end=" ")
+         print(printval.data, end = " ")
          printval = printval.next
 
    def AddAtBeginning(self, newdata):
@@ -23,21 +19,43 @@ class addVal:
       NewNode = Node(newdata)
       if (self.head == None):
          self.head = NewNode
-         return
       last = self.head
-      while last.next:
+      while(last.next):
          last = last.next
       last.next = NewNode
-      
+
+   def AddAtMiddle(self, newdata):
+      NewNode = Node(newdata)
+
+      length = 0
+      temp = self.head
+      while(temp):
+         temp = temp.next
+         length += 1
+
+      mid = length // 2
+
+      temp = self.head
+
+      for _ in range(mid-1):
+         temp = temp.next
+
+      NewNode.next = temp.next
+      temp.next = NewNode
 
 l1 = addVal()
-l1.head = Node("53")
-e2 = Node("65")
-e3 = Node("75")
+l1.head = Node(56)
+e2 = Node(75)
+e3 = Node(88)
 l1.head.next = e2
 e2.next = e3
 
-l1.listprint()
-l1.AddAtBeginning("69")
-l1.AddAtEnd("96")
-l1.listprint()
+print("Linked List: ")
+l1.listing()
+
+l1.AddAtBeginning(12)
+l1.AddAtEnd(99)
+l1.AddAtMiddle(5)
+
+print("\nLinked List after Adding new elements: ")
+l1.listing()
